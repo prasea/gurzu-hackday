121218c827b4d26128afbf7fcbdc74e6b9c79225
prasea <prajanya.stha7@gmail.com>
2024-09-13 14:01:39 +0545

    Use nested model form for ParkSpace & Vehicle

--- app/controllers/park_spaces_controller.rb
+++ app/controllers/park_spaces_controller.rb
@@ -7,14 +7,18 @@ class ParkSpacesController < ApplicationController
 
   def new
     @park_space = ParkSpace.new
-    2.times { @park_space.vehicles.build }
+    @park_space.vehicles.build
+    # 2.times { @park_space.vehicles.build }
   end
 
   def create
     @park_space = ParkSpace.new(park_space_params)
     @park_space.user = current_user
-    # Geocode the location to get latitude and longitude
-    geocode_location(@park_space)
+    # Geocode the location to get latitude and longitude if not supplied from form 
+    if (!@park_space.latitude && !@park_space.longitude) 
+      geocode_location(@park_space)
+    end
+
     if @park_space.save
       redirect_to park_spaces_path,  notice: "Park space was successfully created."
     else
@@ -22,10 +26,26 @@ class ParkSpacesController < ApplicationController
     end
   end
 
+  def edit 
+    @park_space = ParkSpace.find(params[:id])  
+  end
+
+  def update 
+    @park_space = ParkSpace.find(params[:id])
+    if @park_space.update(park_space_params)
+      # redirect_to park_space_path(@park_space)
+      redirect_to park_spaces_path, notice: "Park space updated !"
+    else 
+      render :edit, status: :unprocessable_entity, notice: 'Unable to update park space'
+    end
+  end
+
+
+
 
   private
   def park_space_params
-    params.require(:park_space).permit(:name, :location, :user_id,
+    params.require(:park_space).permit(:name, :location, :latitude, :longitude,  :user_id,
                                        vehicles_attributes: [ :id, :vehicle_name, :capacity, :hourly_rate, :status, :_destroy ])
   end
 
@@ -37,8 +57,8 @@ class ParkSpacesController < ApplicationController
       park_space.longitude = coordinates[1]
     else
       # Handle cases where location cannot be geocoded, e.g., set defaults or raise an error
-      park_space.latitude = nil
-      park_space.longitude = nil
+      park_space.latitude = 26.6686775
+      park_space.longitude = 87.6767778
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
     end
     link_to(name, "#", class: html_options[:class], id: html_options[:id], data: { fields: fields.gsub("\n", ""), association_insertion_node: html_options[:data][:association_insertion_node], association_insertion_method: html_options[:data][:association_insertion_method] }.merge(html_options))
   end
+
+  def link_to_add_fields(name, f, association)
+    # Buld an instance of association record.  For ParkSpace instance (f), calling association instance (:vehicles),  klass is the Vehcile model, calling new to build Vehicle instance 
+    new_object = f.object.send(association).klass.new 
+    # To generate unique random value
+    id = new_object.object_id 
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + '_fields', f:builder)
+    end
+    link_to(name, '#', class: 'add_fields btn btn-outline-secondary mb-3', data: {id: id, fields: fields.gsub("\n", "")} )
+  end
 end
